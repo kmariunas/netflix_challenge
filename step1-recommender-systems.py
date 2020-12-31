386a4a0f2cb0637e07ed8b4642090d7ec2b49dab
Karolis Mariunas <kmariunas@gmail.com>
2020-12-31 14:08:52 +0200

    collaborative filtering 0.1

--- step1-recommender-systems.py
+++ step1-recommender-systems.py
@@ -114,20 +114,40 @@ def normalize_matrix(utility_matrix):
 # Calculates cosine similarity between two movies.
 def calculate_similarity(row1, row2):
     # cos α = A dot B / (|A| * |B|)
-    return np.dot(row1, row2) / (np.linalg.norm(row1) * np.linalg.norm(row2))
+    norm = np.linalg.norm(row1) * np.linalg.norm(row2)
+    if norm == 0:
+        return -1
+    return np.dot(row1, row2) / norm
 
 
 # Returns array
 def calculate_movies_similarity(utility_matrix):
     # len(utility_matrix) = number of movies
     similarities = np.empty((len(utility_matrix), len(utility_matrix)))
-    for i in range(1, len(utility_matrix)-2):
+    for i in range(1, len(utility_matrix)-1):
         for j in range(i+1, len(utility_matrix)):
             sim = calculate_similarity(utility_matrix[i], utility_matrix[j])
+            if sim > 1 or sim < -1:
+                print("deep shit or dipshit")
             similarities[i, j] = sim
             similarities[j, i] = sim
     return similarities
 
+# find most similar movies of the user similar to movie i
+def get_n_similar(normalized_matrix, user, similarity_matrix, n, i):
+    row = similarity_matrix[i]
+    ind = np.argsort(-1 * row)[:len(row)]
+    print(ind)
+    most_similar = np.empty((n, ))
+    i = 0
+    for index in ind:
+        if i == n - 1:
+            break
+        if normalized_matrix[index, user] != 0:
+            most_similar[i] = index
+            i += 1
+    print(most_similar)
+    return most_similar
 
 # # TESTING SIMILARITY:
 # row1 = np.array([4, 0, 0, 5, 1, 0, 0])
@@ -139,14 +159,14 @@ def calculate_movies_similarity(utility_matrix):
 utility_matrix = create_utility_matrix(users_description.to_numpy(),
                                        movies_description.to_numpy(),
                                        ratings_description.to_numpy())
-print(utility_matrix)
-normalized_matrix = normalize_matrix(utility_matrix)
-print("Normalized matrix:")
-print(normalized_matrix)
-movies_similarities = calculate_movies_similarity(normalized_matrix)
-print("Similarities:")
-# I think the length of some rows is close to zero and division causes an error.
-print(movies_similarities)
+# print(utility_matrix)
+# normalized_matrix = normalize_matrix(utility_matrix)
+# print("Normalized matrix:")
+# print(normalized_matrix)
+# movies_similarities = calculate_movies_similarity(normalized_matrix)
+# print("Similarities:")
+# # I think the length of some rows is close to zero and division causes an error.
+# print(movies_similarities)
 
 #####
 ##
@@ -156,8 +176,27 @@ print(movies_similarities)
 
 def predict_collaborative_filtering(movies, users, ratings, predictions):
     # TO COMPLETE
+    utility_matrix = create_utility_matrix(users.to_numpy(), movies.to_numpy(), ratings.to_numpy())
+    normalized_matrix = normalize_matrix(utility_matrix)
+    movie_similarities = calculate_movies_similarity(normalized_matrix)
 
-    pass
+    predictions_matrix = np.empty((len(predictions), 2))
+    predictions_np = predictions.to_numpy()
+    i = 0
+
+    for row in predictions_np:
+        similar_ind = get_n_similar(normalized_matrix, row[0], movie_similarities, 10, row[1])
+        predictions_matrix[i, 0] = i + 1
+
+        rating_sim = 0
+        sim_sum = 0
+        for item in similar_ind:
+            if item is not 0:
+                rating_sim += movie_similarities[row[1], item] * utility_matrix[item, row[0]]
+                sim_sum += movie_similarities[row[1], item]
+        predictions_matrix[i, 1] = rating_sim / sim_sum
+        i += 1
+    return predictions_matrix
 
 
 #####
@@ -205,7 +244,7 @@ def predict_random(movies, users, ratings, predictions):
 #####    
 
 ## //!!\\ TO CHANGE by your prediction function
-predictions = predict_random(movies_description, users_description, ratings_description, predictions_description)
+predictions = predict_collaborative_filtering(movies_description, users_description, ratings_description, predictions_description)
 
 # Save predictions, should be in the form 'list of tuples' or 'list of lists'
 with open(submission_file, 'w') as submission_writer:
